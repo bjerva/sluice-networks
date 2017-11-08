620bc244dbc9e9d2d53ddc42e261db37e60f9b22
Johannes Bjerva <johannesbjerva@gmail.com>
2017-11-08 15:10:20 +0100

    Expanded to more langs

--- generate_ttt_runs.py
+++ generate_ttt_runs.py
@@ -24,7 +24,7 @@ base_slurm = '''#!/bin/bash
 # parser.add_argument('--n-runs', type=int, default=5)
 # args = parser.parse_args()
 
-base_python = '''python -u -O run_sluice_net.py --dynet-autobatch 1 --dynet-seed 123 --task-names pos --h-layers 1 --pred-layer 1  --patience 3 --train-dir ~/data/ --dev-dir ~/data/ --test-dir ~/data/ --train {0} --test {1} --constrain-matrices 0 --in-dim 1 --model-dir ./models/{0}_{1}_$SLURM_ARRAY_TASK_ID --log-dir ./logs/ --lemb-dir 081117/{0}_{1}_$SLURM_ARRAY_TASK_ID_'''
+base_python = '''python -u -O run_sluice_net.py --dynet-autobatch 1 --dynet-seed 123 --task-names pos --h-layers 1 --pred-layer 1  --patience 3 --train-dir ~/data/ --dev-dir ~/data/ --test-dir ~/data/ --train {0} --test {1} --constrain-matrices 0 --in-dim 1 --model-dir ./models/{3}_{1}_$SLURM_ARRAY_TASK_ID --log-dir ./logs/ --lemb-dir 081117/{3}_{1}_$SLURM_ARRAY_TASK_ID_'''
 log_dir = ' > ~/ttt_logs/$SLURM_ARRAY_TASK_ID/{0}_{1}.txt'
 
 languages = 'UD_Finnish UD_Estonian UD_Hungarian UD_NorthSami'.split()
@@ -33,5 +33,23 @@ for l1 in languages:
     for l2 in languages:
         with open('ttt_runs/{0}_{1}.sh'.format(l1, l2), 'w') as out_f:
             out_f.write(base_slurm.format(l1[3:6]+'_'+l2[3:6]))
-            out_f.write(base_python.format(l1, l2))
+            out_f.write(base_python.format(l1, l2, l1))
             out_f.write(log_dir.format(l1, l2))
+
+for l1 in languages:
+    for l2 in languages:
+        for l3 in languages:
+            with open('ttt_runs/{0}_{1}_{2}.sh'.format(l1, l2, l3), 'w') as out_f:
+                out_f.write(base_slurm.format(l1[3:6]+'_'+l3[3:6]))
+                out_f.write(base_python.format(l1+' '+l2, l3, l1+'_'+l2))
+                out_f.write(log_dir.format(l1+'_'+l2, l3))
+
+
+for l1 in languages:
+    for l2 in languages:
+        for l3 in languages:
+            for l4 in languages:
+                with open('ttt_runs/{0}_{1}_{2}_{3}.sh'.format(l1, l2, l3, l4), 'w') as out_f:
+                    out_f.write(base_slurm.format(l1[3:6]+'_'+l4[3:6]))
+                    out_f.write(base_python.format(l1+' '+l2+' '+l3, l4, l1+'_'+l2+'_'+l3))
+                    out_f.write(log_dir.format(l1+'_'+l2+'_'+l3, l4))
